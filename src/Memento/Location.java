36c54d83c500d8ae063724e6ae5e60fb5a0ffae6
Shane McGinley <shane.mcginley@students.ittralee.ie>
2020-11-27 00:41:31 +0000

    Before Memento Pattern - Added two classes that were done previously and never committed.

---
+++ src/Memento/Location.java
@@ -0,0 +1,9 @@
+package Memento;
+
+public class Location {
+    public final String WHD = "Winterhold";
+    public final String WHM = "Windhelm";
+    public final String WRN = "Whiterun";
+    public final String KL = "King's Landing";
+    public final String WF = "Winterfell";
+}
---
+++ src/Memento/WeaponType.java
@@ -0,0 +1,9 @@
+package Memento;
+
+public class WeaponType {
+    public final String SNIPER_RIFLE = "Sniper Rifle";
+    public final String ASSAULT_RIFLE = "Assault Rifle";
+    public final String SUBMACHINEGUN = "Submachine Gun";
+    public final String SHOTGUN = "Shotgun";
+    public final String KNIFE = "Knife";
+}
